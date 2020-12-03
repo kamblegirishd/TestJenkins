9e354109ab801719ab60d78d5d8634e20ef119fa
girishdkamble@gmail.com <girishdkamble@gmail.com>
2020-12-03 11:33:13 +0530

    First commit

---
+++ TestJenkins/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(CreateMessage());
+            Console.ReadLine();
+        }
+
+        private static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
